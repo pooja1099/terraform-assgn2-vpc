7b02b8448a2d70cfca42efdc2a27d277b9eaa073
pooja1099 <poojasangwan0510@gmail.com>
2023-02-17 23:20:20 +0530

    first commit

---
+++ variable.tf
@@ -0,0 +1,18 @@
+variable "AWS_REGION" {    
+    default = "us-west-2"
+}
+
+# variable "var-subnet" {
+#   default = 2
+# }
+#variable for instance-1
+variable "var-inst1" {
+  default = 2
+}
+variable "var-inst2" {
+  default = 2
+}
+
+
+
+
